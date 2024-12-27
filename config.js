9a0bda830aa5befbb892e99a8aacca7490dfc275
DasunMax970 <dasunmax08@gmail.com>
2024-12-27 20:49:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: 'DARK-SHUTER-𝙼𝙳=P75QnRZB#IsqwBdGfToyEQoQ_HeeHB_euyzB0gX_c2rlI-zYmb7Y',
+SESSION_ID: 'DARK-SHUTER-MD=ZvETjAZL#423GJHAGyuCAR_XBC55ndCBxk5d3uWTSTKDsFlRac5Y',
 GITHUB_AUTH_TOKEN: 'Jhs8hjpOdvByrNOeaWYEJHSj707mCb1JGoTx', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
 GITHUB_USER_NAME: 'DasunMax970',
 
