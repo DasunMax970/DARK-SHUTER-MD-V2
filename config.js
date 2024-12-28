f626c27f4457b7cd077a204338ac65bb27255023
DasunMax970 <dasunmax08@gmail.com>
2024-12-28 11:16:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: 'DARK-SHUTER-MD=mU51mR5a#-B0ulKQaoIgzc_TOEwyg0PhdkDfwOG9rKCHU_xbpAuc',
-GITHUB_AUTH_TOKEN: '0W6CuKH4TmZGsVy3BNtiGo8QU6BiJb3zUbf4', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
-GITHUB_USER_NAME: 'uvindugit',
+GITHUB_AUTH_TOKEN: 'ypUCG7aBpzwNdOaLJvm44D3gecnTzI0zphFK', //"ghp_ " මෙම කොටස ඉවත් කර token එක දාන්න.
+GITHUB_USER_NAME: 'hukahanheroku',
 
 };
